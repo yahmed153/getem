c048632fbc51758568dae12f3cb2d57968cc146c
twcrone <twcrone@gmail.com>
2026-04-18 10:19:59 -0400

    Add GitemUser record

---
+++ src/main/java/twcrone/gitem/GitemUser.java
@@ -0,0 +1,20 @@
+package twcrone.gitem;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+public record GitemUser(
+    @JsonProperty("user_name") String userName,
+    @JsonProperty("display_name") String displayName,
+    @JsonProperty("avatar") String avatar,
+    @JsonProperty("geo_location") String geoLocation,
+    String email,
+    String url,
+    @JsonProperty("created_at") String createdAt,
+    List<Repo> repos
+) {}
+
+record Repo(
+    String name,
+    String url
+) {}

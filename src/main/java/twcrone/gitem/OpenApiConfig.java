f7f8da09ad53c65feb6e041a48c8ef923d50d6b2
twcrone <twcrone@gmail.com>
2026-04-18 12:24:18 -0400

    Add SWAGGER config

---
+++ src/main/java/twcrone/gitem/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package twcrone.gitem;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gitem API")
+                        .version("1.0")
+                        .description("The Gitem API"));
+    }
+}

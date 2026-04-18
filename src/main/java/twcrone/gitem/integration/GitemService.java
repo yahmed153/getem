734f55e84d033f02c55edf071443e0bd6e8e24db
twcrone <twcrone@gmail.com>
2026-04-18 10:53:34 -0400

    Stub GitemService to use GithubService

--- src/main/java/twcrone/gitem/integration/GitemService.java
+++ src/main/java/twcrone/gitem/integration/GitemService.java
@@ -4,9 +4,16 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import twcrone.gitem.api.GitemRepo;
 import twcrone.gitem.api.GitemUser;
+import twcrone.gitem.external.GithubService;
 
 @Service
 public class GitemService {
+
+    private final GithubService githubService;
+
+    public GitemService(GithubService githubService) {
+        this.githubService = githubService;
+    }
 
     public Mono<GitemUser> getem(String userId) {
         return Mono.just(new GitemUser(

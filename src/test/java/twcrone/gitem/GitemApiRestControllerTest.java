449c38767d89e75606558baa6d6453eafa747611
twcrone <twcrone@gmail.com>
2026-04-18 13:37:46 -0400

    Remove unneeded Redis password

--- src/test/java/twcrone/gitem/GitemApiRestControllerTest.java
+++ src/test/java/twcrone/gitem/GitemApiRestControllerTest.java
@@ -26,44 +26,10 @@ public class GitemApiRestControllerTest {
     static void redisProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.data.redis.host", redis::getHost);
         registry.add("spring.data.redis.port", redis::getFirstMappedPort);
-        registry.add("spring.data.redis.password", () -> "mysecretpassword");
     }
 
     @Autowired
     private WebTestClient webTestClient;// = WebTestClient.bindToServer().baseUrl("http://localhost:8080").build();
-
-    @Autowired
-    private JsonMapper jsonMapper;
-
-
-//    @Autowired
-//    private GithubService githubService;
-//
-//    private GithubUser mockUser;
-//    private List<GithubRepo> mockRepos;
-//
-//    @TestConfiguration
-//    static class TestConfig {
-//        @Bean
-//        @Primary
-//        public GithubService githubService() {
-//            return Mockito.mock(GithubService.class);
-//        }
-//    }
-
-//    @BeforeEach
-//    void setUp() throws IOException {
-//        // Load test data from resources
-//        String userJson = Files.readString(Path.of("src/test/resources/user.json"));
-//        String reposJson = Files.readString(Path.of("src/test/resources/repos.json"));
-//
-//        mockUser = jsonMapper.readValue(userJson, GithubUser.class);
-//        mockRepos = jsonMapper.readValue(reposJson, jsonMapper.getTypeFactory().constructCollectionType(List.class, GithubRepo.class));
-//
-//        // Mock the GithubService
-//        when(githubService.getUser(anyString())).thenReturn(Mono.just(mockUser));
-//        when(githubService.getReposFor(anyString())).thenReturn(Mono.just(mockRepos));
-//    }
 
     @Test
     void testGetUser() {

f47cd55984e8f5455deb1cfe4cf4bce854e75c2c
twcrone <twcrone@gmail.com>
2026-04-19 14:19:17 -0400

    Add error case fix not found

--- src/main/java/twcrone/gitem/api/RestConrollerAdvice.java
+++ src/main/java/twcrone/gitem/api/RestConrollerAdvice.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
 @RestControllerAdvice
@@ -11,6 +12,16 @@ public class RestConrollerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
     public Mono<ResponseEntity<ErrorResponse>> handleGeneralError(RuntimeException runtimeException) {
+        ErrorResponse error = new ErrorResponse("SERVICE_UNAVAILABLE", "Github API is not available at this time");
+        return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(error));
+    }
+
+    @ExceptionHandler(WebClientResponseException.class)
+    public Mono<ResponseEntity<ErrorResponse>> handleWebClientExceptions(WebClientResponseException webclientException) {
+        if(HttpStatus.NOT_FOUND.equals(webclientException.getStatusCode())) {
+            var error = new ErrorResponse("NOT_FOUND", "Github does not have that user");
+            return Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(error));
+        }
         ErrorResponse error = new ErrorResponse("SERVICE_UNAVAILABLE", "Github API is not available at this time");
         return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(error));
     }
